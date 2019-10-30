9f1ca35c324a9c1e29a449ba6af6e46894ff6009
Miy4bi <0tn0090078z333v@gmail.com>
2019-10-30 13:56:25 +0900

    changed company setting

--- init.el
+++ init.el
@@ -72,7 +72,7 @@
   (add-hook 'after-init-hook 'global-company-mode)
   (setq company-idle-delay 0)
   (setq company-echo-delay 0)
-  (setq company-minimum-prefix-length 2)
+  (setq company-minimum-prefix-length 1)
   (setq company-selection-wrap-around t)
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
