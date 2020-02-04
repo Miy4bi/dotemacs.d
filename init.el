ba9f62ea40fb624711ab948e109259e6abe368a8
Miy4bi <0tn0090078z333v@gmail.com>
2020-02-05 06:19:59 +0900

    update

--- init.el
+++ init.el
@@ -70,11 +70,17 @@
   (setq company-echo-delay 0)
   (setq company-minimum-prefix-length 1)
   (setq company-selection-wrap-around t)
-  (define-key company-active-map (kbd "M-n") nil)
-  (define-key company-active-map (kbd "M-p") nil)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (define-key company-active-map (kbd "C-h") nil))
+  :bind
+  (:map company-active-map
+        ("M-n" . nil)
+        ("M-p" . nil)
+        ("C-n" . 'company-select-next)
+        ("C-p" . 'company-select-previous)
+        ("<tab>" . 'company-complete-selection))
+  (:map company-search-map
+        ("C-n" . 'company-select-next)
+        ("C-p" . 'company-select-previous))
+  )
 
 (use-package company-c-headers
   :ensure t
